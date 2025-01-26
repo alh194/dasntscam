824aad48b610f31c4931c850fc278707e6fd55a6
alh194 <Alberto.Lopez31@alu.uclm.es>
2025-01-26 12:55:37 +0100

    Jwt Service(generar token con io.jsonwebtoken), metodo registro en authService, ApplicationConfig para verrificar user

--- src/main/java/com/themistech/dasntscam/Auth/AuthService.java
+++ src/main/java/com/themistech/dasntscam/Auth/AuthService.java
@@ -1,17 +1,40 @@
 package com.themistech.dasntscam.Auth;
 
+import com.themistech.dasntscam.Jwt.JwtService;
+import com.themistech.dasntscam.User.Role;
+import com.themistech.dasntscam.User.User;
+import com.themistech.dasntscam.User.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
     public AuthResponse login(LoginRequest request) {
         return null;
     }
 
     public AuthResponse register(RegisterRequest request) {
-        return null;
+        //Recuperamos los datos del request de registro
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode( request.getPassword()))
+                .firstname(request.getFirstname())
+                .lastname(request.lastname)
+                .country(request.getCountry())
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        //Devolvemos el response personalizado con el token
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .build();
     }
 }
--- src/main/java/com/themistech/dasntscam/Config/SecurityConfig.java
+++ src/main/java/com/themistech/dasntscam/Config/SecurityConfig.java
@@ -1,18 +1,23 @@
 package com.themistech.dasntscam.Config;
 
+import com.themistech.dasntscam.Jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,12 +25,18 @@ public class SecurityConfig {
         return http
                 .csrf(csrf ->
                         csrf.disable())
+                //Endpoins publicos
                 .authorizeHttpRequests(authRequest ->
                         authRequest
                                 .requestMatchers("/auth/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults())
+                //Autenticacion por JWT
+                .sessionManagement(sessionManager->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
